f7a88d5fc6ea36f84f785770282cc7e5f3a67de5
Andrew Chao <andrew@Andrews-MacBook-Pro-2.local>
2014-09-28 23:03:23 -0700

    Initial Commit

---
+++ twitter-wk3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter-wk3
+//
+//  Created by Andrew Chao on 9/28/14.
+//  Copyright (c) 2014 Andrew Chao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ twitter-wk3Tests/twitter_wk3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  twitter_wk3Tests.swift
+//  twitter-wk3Tests
+//
+//  Created by Andrew Chao on 9/28/14.
+//  Copyright (c) 2014 Andrew Chao. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class twitter_wk3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
